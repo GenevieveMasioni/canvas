f9ae14f9d0e9ed1b46e87d956fac254fc34917ed
Geneviève Masioni <contact@genevievemasioni.com>
2024-01-12 01:34:37 +0100

    Refactor: clean and optimised JS code

---
+++ assets/js/classes.js
@@ -0,0 +1,188 @@
+class DrawingBoard {
+    canvas; ctx; canvasStatesList;
+    currentCanvasStateIdx;
+    lastX; lastY; isDrawing;
+    projectName;
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+        this.canvasStatesList = [];
+        this.currentCanvasStateIdx = 0;
+        this.lastX = 0;
+        this.lastY = 0;
+        this.isDrawing = false;
+        this.projectName = "default";
+        this.maxStatesLimit = 10;
+        this.init();
+        this.saveAction();
+    }
+
+    stopDrawing() {
+        this.isDrawing = false;
+        this.saveAction();
+    }
+
+    startDrawing(e) {
+        this.isDrawing = true;
+        [this.lastX, this.lastY] = [e.offsetX, e.offsetY];
+    }
+    
+    init() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx.lineJoin = 'round';
+        this.ctx.lineCap = 'round';
+
+        this.canvas.addEventListener("mousemove", this.drawLine.bind(this));
+        this.canvas.addEventListener("mousedown", this.startDrawing.bind(this));
+    
+        this.canvas.addEventListener("mouseup", this.stopDrawing.bind(this));
+        this.canvas.addEventListener("mouseout", this.stopDrawing.bind(this));
+    
+        this.canvas.addEventListener("dragenter", this.ignoreMouseEvent);
+        this.canvas.addEventListener("dragover", this.ignoreMouseEvent);
+        this.canvas.addEventListener("drop", this.handleImageDrop.bind(this));
+    }
+
+    setLineWidth(value) {
+        this.ctx.lineWidth = value;
+    }
+
+    setStrokeColor(value) {
+        this.ctx.strokeStyle = value;
+    }
+
+    clear(save = true) {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        if(!save) return;
+        this.saveAction();
+    }
+
+    drawLine(e) {
+        if (!this.isDrawing) return;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.lastX, this.lastY);
+        this.ctx.lineTo(e.offsetX, e.offsetY);
+        this.ctx.stroke();
+        [this.lastX, this.lastY] = [e.offsetX, e.offsetY];
+    }
+
+    ignoreMouseEvent(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    drawImage(source, x, y, width, height) {
+        const image = new Image();
+        image.onload = () => {
+            width = width ? width : image.naturalWidth;
+            height = height ? height : image.naturalHeight;
+            this.ctx.drawImage(image, x, y, width, height);
+        }
+        image.src = source;
+    }
+
+    handleImageDrop(e) {
+        this.ignoreMouseEvent(e);
+        const currentX = e.offsetX;
+        const currentY = e.offsetY;
+
+        const dt = e.dataTransfer;
+        const files = dt.files;
+        const file = files[0];
+
+        if(!file.type.startsWith("image/")) {
+            console.error("Not an image");
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onerror = (error) => {
+            console.error("Error while reading file :", error.message);
+        };
+        reader.onload = (e) => {
+            this.drawImage(e.target.result, currentX, currentY);
+            this.saveAction();
+        };
+        reader.readAsDataURL(file);
+    }
+
+    setProjectName(name) {
+        this.projectName = name;
+    }
+
+    download() {
+        const dataURL = this.canvas.toDataURL();
+        const link = document.createElement("a");
+        link.download = `${this.projectName}.png`;
+        link.href = dataURL;
+        link.click();
+    }
+
+    saveProject() {
+        const project = {
+            "name": this.projectName,
+            "data": this.canvas.toDataURL()
+        };
+
+        const blob = new Blob([JSON.stringify(project, null, 2)], {
+            type: "application/json",
+        });
+        const projectURL = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = `${this.projectName}.json`;
+        link.href = projectURL;
+        link.click();
+        URL.revokeObjectURL(projectURL);
+    }
+
+    openProject(file) {
+        if(!file || !file.type.startsWith("application/json")) {
+            console.error("No file or not a JSON file");
+            return;
+        }
+        var reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+        reader.onerror = (error) => {
+            console.error("Error while reading file :", error.message);
+        };
+        reader.onload = readerEvent => {
+            var content = readerEvent.target.result;
+            const projectData = JSON.parse(content);
+            this.drawImage(projectData.data, 0, 0, this.canvas.width, this.canvas.height);
+            this.setProjectName(projectData.name);
+        }
+    }
+
+    renderState(index) {
+        this.clear(false);
+        const state = this.canvasStatesList[index];
+        this.drawImage(state, 0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    undoAction() {
+        this.currentCanvasStateIdx = this.currentCanvasStateIdx > 0 ? 
+        --this.currentCanvasStateIdx : 0;
+        this.renderState(this.currentCanvasStateIdx);
+    }
+
+    redoAction() {
+        if (this.currentCanvasStateIdx == this.canvasStatesList.length - 1) return;
+        this.currentCanvasStateIdx++;
+        this.renderState(this.currentCanvasStateIdx);
+    }
+
+    saveAction() {
+        while(this.canvasStatesList.length > this.maxStatesLimit) {
+            const state = this.canvasStatesList.shift();
+            URL.revokeObjectURL(state);
+        }
+
+        this.canvas.toBlob((blob) => {
+            const currentState = URL.createObjectURL(blob);
+            this.canvasStatesList.push(currentState);
+            this.currentCanvasStateIdx = this.canvasStatesList.length - 1;
+        });
+    }
+}
---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+window.onload = () => {
+  const canvas = document.getElementById("drawing-area");
+  const drawingBoard = new DrawingBoard(canvas);
+
+  const projectNameInput = document.getElementById("project-name");
+  const clearBtn = document.getElementById("toolbar-eraser-all");
+  const downloadBtn = document.getElementById("toolbar-download");
+  const saveProjectBtn = document.getElementById("toolbar-save-project");
+  const openProjectBtn = document.getElementById("toolbar-open-project");
+  const sizePicker = document.getElementById("size-picker");
+  const colorPicker = document.getElementById("color-picker");
+  const filePicker = document.createElement('input');
+  const undoBtn = document.getElementById("toolbar-undo");
+  const redoBtn = document.getElementById("toolbar-redo");
+  var selectedBackgroundPattern;
+
+  function initializetUI() {
+    filePicker.type = 'file';
+    sizePicker.value = 3;
+    colorPicker.value = "#373737";
+    drawingBoard.setProjectName(projectNameInput.value);
+  }
+
+  function openFilePicker() {
+    filePicker.click();
+  }
+
+  function handleKeyboardShortcut(e) {
+    if (e.ctrlKey && e.code == "KeyW") {
+      drawingBoard.undoAction();
+    }
+
+    if (e.ctrlKey && e.code == "KeyY") {
+      drawingBoard.redoAction();
+    }
+
+    if (e.code == "KeyE") {
+      drawingBoard.clear();
+    }
+
+    if (e.ctrlKey && e.code == "KeyS") {
+      drawingBoard.saveProject();
+    }
+
+    if (e.ctrlKey && e.code == "KeyO") {
+      openFilePicker();
+    }
+
+    if (e.ctrlKey && e.code == "KeyD") {
+      drawingBoard.download();
+    }
+  }
+  
+  function handleColorChoice(e) {
+    colorPicker.value = `${e.target.dataset.value}`;
+  }
+
+  function handleBackgroundPatternChoice(e) {
+    const pattern = e.target;
+    if (selectedBackgroundPattern) {
+      selectedBackgroundPattern.classList.remove("selected");
+    }
+    pattern.classList.add("selected");
+    selectedBackgroundPattern = pattern;
+    document.body.style.backgroundImage = `url(${pattern.dataset.url})`;
+  }
+
+  function setupEventListeners() {
+    document.querySelectorAll(".stroke-color-choice")
+      .forEach((colorChoice) => {
+        colorChoice.style.backgroundColor = `${colorChoice.dataset.value}`;
+        colorChoice.addEventListener('click', handleColorChoice);
+      });
+
+    document.querySelectorAll(".toolbar-background-pattern-choice")
+      .forEach((pattern) => {
+        pattern.addEventListener('click', handleBackgroundPatternChoice);
+      });
+
+    sizePicker.addEventListener('change', 
+    (e) => drawingBoard.setLineWidth(sizePicker.value));
+    colorPicker.addEventListener('change', 
+    (e) => drawingBoard.setStrokeColor(colorPicker.value));
+
+    clearBtn.addEventListener("click", (e) => drawingBoard.clear());
+    downloadBtn.addEventListener('click', (e) => drawingBoard.download());
+    filePicker.addEventListener("change", 
+    (e) => drawingBoard.openProject(e.target.files[0]));
+    projectNameInput.addEventListener("change", 
+    (e) => drawingBoard.setProjectName(e.target.value));
+
+    saveProjectBtn.addEventListener('click', (e) => drawingBoard.saveProject());
+    openProjectBtn.addEventListener('click', openFilePicker);
+
+    undoBtn.addEventListener('click', (e) => drawingBoard.undoAction());
+    redoBtn.addEventListener('click', (e) => drawingBoard.redoAction());
+
+    window.addEventListener('keydown', handleKeyboardShortcut);
+  }
+
+  initializetUI();
+  setupEventListeners();
+}
